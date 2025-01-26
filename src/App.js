dd9762eecda6586b58ea71c050bd05b7fa0639cf
kianoding <kkiantor@pratt.edu>
2025-01-25 19:17:43 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import MaterialViewer from './components/MaterialViewer';
+
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Material Lab Studio</h1>
+      <MaterialViewer />
+    </div>
+  );
+}
+
+export default App;
